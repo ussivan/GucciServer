f4477e80510bb00e2aa637cb690dd6bbd97098e0
Ivan Uss <iuss@devexperts.com>
2022-11-08 02:18:08 +0300

    working server with database

---
+++ src/main/kotlin/by/iuss/gucciserver/GucciServerApplication.java
@@ -0,0 +1,12 @@
+package by.iuss.gucciserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GucciServerApplication {
+
+    public static void main(String... args) {
+        SpringApplication.run(GucciServerApplication.class, args);
+    }
+}
---
+++ src/main/kotlin/by/iuss/gucciserver/KalRepository.java
@@ -0,0 +1,8 @@
+package by.iuss.gucciserver;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+interface KalRepository extends JpaRepository<Kal, Long> {
+
+}
